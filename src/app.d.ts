b46151b979143c6c578d200662e91ebcee9bbb53
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-08-27 14:39:50 +0100

    clone cmsass

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,4 @@
-import { SupabaseClient, Session } from "@supabase/supabase-js"
+import { SupabaseClient, Session, User } from "@supabase/supabase-js"
 import { Database } from "./DatabaseDefinitions"
 
 // See https://kit.svelte.dev/docs/types#app
--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -17,8 +17,8 @@ export const pricingPlans = [
       "A plan to test the purchase experience. Try buying this with the test credit card 4242424242424242.",
     price: "$5",
     priceIntervalName: "per month",
-    stripe_price_id: "price_1NkdZCHMjzZ8mGZnRSjUm4yA",
-    stripe_product_id: "prod_OXj1CcemGMWOlU",
+    stripe_price_id: "price_1PsKXXApN1csWalssDVkvNDD",
+    stripe_product_id: "prod_Qjo9lzIwTezpcE",
     features: [
       "Everything in Free",
       "Support us with fake money",
@@ -32,8 +32,8 @@ export const pricingPlans = [
       "A plan to test the upgrade experience. Try buying this with the test credit card 4242424242424242.",
     price: "$15",
     priceIntervalName: "per month",
-    stripe_price_id: "price_1Nkda2HMjzZ8mGZn4sKvbDAV",
-    stripe_product_id: "prod_OXj20YNpHYOXi7",
+    stripe_price_id: "price_1PsKZBApN1csWalsniyNDRjs",
+    stripe_product_id: "prod_QjoBZ36PfGidUS",
     features: [
       "Everything in Pro",
       "Try the 'upgrade plan' UX",
