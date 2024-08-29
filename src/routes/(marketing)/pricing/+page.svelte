c7ffcff827a05b7e9c6da8fab7656e69de90b61f
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-08-29 19:02:29 +0100

    use shadcn for the pricing_module

--- src/routes/(marketing)/pricing/+page.svelte
+++ src/routes/(marketing)/pricing/+page.svelte
@@ -55,7 +55,7 @@
 
 <div class="min-h-[70vh] pb-8 pt-[5vh] px-4">
   <h1 class="text-3xl font-bold text-center">Pricing</h1>
-  <h2 class="text-xl text-center text-slate-500 mt-1 pb-3">
+  <h2 class="text-xl text-center text-muted-foreground mt-1 pb-3">
     Totally free, scale to millions of users
   </h2>
 
