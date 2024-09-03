a5a7dc01c4d464fb04ffb2318ff2dcdbdff7096b
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-09-03 10:00:12 +0100

    convert blog to shadcn

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -4,7 +4,6 @@
   import * as DropDownMenu from "$lib/components/ui/dropdown-menu"
 
   import { WebsiteName } from "./../../config"
-  import "../../app.css"
 </script>
 
 <div class="flex py-4 bg-primary text-primary-foreground container mx-auto">
