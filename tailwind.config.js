8fced46d5e6633429e38e694534aaf164ae3dffb
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-09-03 10:54:45 +0100

    finish up shadcn conversion

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,7 @@ const config = {
   darkMode: ["class"],
   content: ["./src/**/*.{html,js,svelte,ts}"],
   safelist: ["dark"],
-  plugins: [
-    require("@tailwindcss/typography"), 
-    require("daisyui")
-  ],
+  plugins: [require("@tailwindcss/typography")],
   theme: {
     container: {
       center: true,
