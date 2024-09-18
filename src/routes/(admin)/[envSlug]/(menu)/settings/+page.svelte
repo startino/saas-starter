326c388deea71f5994d6ce5f94024004e5583b00
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-09-18 14:59:22 +0100

    hide settings to anonymous user

--- src/routes/(admin)/[envSlug]/(menu)/settings/+page.svelte
+++ src/routes/(admin)/[envSlug]/(menu)/settings/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import SettingsModule from "./settings_module.svelte"
+  import { getEnvironmentState } from "$lib/states/index"
+  import { buttonVariants } from "$lib/components/ui/button"
 
   let { data } = $props()
-  let { session, profile } = data
+  let { profile, user } = data
+
+  const environment = getEnvironmentState()
 </script>
 
 <svelte:head>
@@ -10,63 +14,68 @@
 </svelte:head>
 
 <h1 class="text-2xl font-bold mb-6">Settings</h1>
+{#if !user?.is_anonymous}
+  <SettingsModule
+    title="Profile"
+    editable={false}
+    fields={[
+      { id: "fullName", label: "Name", initialValue: profile?.full_name ?? "" },
+      {
+        id: "companyName",
+        label: "Company Name",
+        initialValue: profile?.company_name ?? "",
+      },
+      {
+        id: "website",
+        label: "Company Website",
+        initialValue: profile?.website ?? "",
+      },
+    ]}
+    editButtonTitle="Edit Profile"
+    editLink="/{environment.slug}/settings/edit_profile"
+  />
 
-<SettingsModule
-  title="Profile"
-  editable={false}
-  fields={[
-    { id: "fullName", label: "Name", initialValue: profile?.full_name ?? "" },
-    {
-      id: "companyName",
-      label: "Company Name",
-      initialValue: profile?.company_name ?? "",
-    },
-    {
-      id: "website",
-      label: "Company Website",
-      initialValue: profile?.website ?? "",
-    },
-  ]}
-  editButtonTitle="Edit Profile"
-  editLink="/account/settings/edit_profile"
-/>
-
-{#if !session?.user.is_anonymous}
   <SettingsModule
     title="Email"
     editable={false}
-    fields={[{ id: "email", initialValue: session?.user?.email || "" }]}
+    fields={[{ id: "email", initialValue: user?.email || "" }]}
     editButtonTitle="Change Email"
-    editLink="/account/settings/change_email"
+    editLink="/{environment.slug}/settings/change_email"
   />
+
+  <SettingsModule
+    title="Password"
+    editable={false}
+    fields={[{ id: "password", initialValue: "••••••••••••••••" }]}
+    editButtonTitle="Change Password"
+    editLink="/{environment.slug}/settings/change_password"
+  />
+
+  <SettingsModule
+    title="Email Subscription"
+    editable={false}
+    fields={[
+      {
+        id: "subscriptionStatus",
+        initialValue: profile?.unsubscribed ? "Unsubscribed" : "Subscribed",
+      },
+    ]}
+    editButtonTitle="Change Subscription"
+    editLink="/{environment.slug}/settings/change_email_subscription"
+  />
+
+  <SettingsModule
+    title="Danger Zone"
+    editable={false}
+    dangerous={true}
+    fields={[]}
+    editButtonTitle="Delete Account"
+    editLink="/{environment.slug}/settings/delete_account"
+  />
+{:else}
+  <p>
+    You need to <a href="/login/sign_up" class={buttonVariants({ size: "sm" })}
+      >sign up</a
+    > to update your settings
+  </p>
 {/if}
-
-<SettingsModule
-  title="Password"
-  editable={false}
-  fields={[{ id: "password", initialValue: "••••••••••••••••" }]}
-  editButtonTitle="Change Password"
-  editLink="/account/settings/change_password"
-/>
-
-<SettingsModule
-  title="Email Subscription"
-  editable={false}
-  fields={[
-    {
-      id: "subscriptionStatus",
-      initialValue: profile?.unsubscribed ? "Unsubscribed" : "Subscribed",
-    },
-  ]}
-  editButtonTitle="Change Subscription"
-  editLink="/account/settings/change_email_subscription"
-/>
-
-<SettingsModule
-  title="Danger Zone"
-  editable={false}
-  dangerous={true}
-  fields={[]}
-  editButtonTitle="Delete Account"
-  editLink="/account/settings/delete_account"
-/>
